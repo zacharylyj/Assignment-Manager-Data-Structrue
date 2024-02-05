e8ff4d1f90278732a8a01ea3f8d90b9b47e4ae25
zacharylyj <2b01.2201861.zacharyleong@gmail.com>
2024-02-05 19:20:48 +0800

    thuis c

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ def main():
     menu.add(controller.evaluate, 3, "Evaluate a single variable")
     menu.add(controller.read_file, 4, "Read assignment statement from file")
     menu.add(controller.sort_file, 5, "Sort assignment statement")
-    menu.add(controller.option1, 6, "Extra Option One")
-    menu.add(controller.option2, 7, "Extra Option Two")
+    menu.add(controller.option1, 6, "Clear Assignments")
+    menu.add(controller.option2, 7, "Extra Option One")
     menu.add(menu.exit_menu, 8, "Exit")
     menu.intro()
 
